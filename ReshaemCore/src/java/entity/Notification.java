61bc15282c6f7c608e8bcbbc5f71bee559f1fe84
Anton Danshin <anton.danshin@frtk.ru>
2012-06-17 17:43:33 +0400

    merge

--- ReshaemCore/src/java/entity/Notification.java
+++ ReshaemCore/src/java/entity/Notification.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
  *
  * @author Danon
  */
-@Entity @Table(name="NTIFICATIONS")
+@Entity @Table(name="NOTIFICATIONS")
 public class Notification implements Serializable {
     private static final long serialVersionUID = 1L;
     
